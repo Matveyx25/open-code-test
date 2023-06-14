3c6bea9c7e2c17419e5e7e88e935696072057c55
Matveyx25 <matveyx25@gmail.com>
2023-06-14 17:53:49 +0400

    Add filters for services

--- src/services/bank-info.service.js
+++ src/services/bank-info.service.js
@@ -1,6 +1,14 @@
 import { instance } from "./axios.config";
 
-export const getAllBankInfo = () => (instance.get('/info'))
+export const getAllBankInfo = (filters) => (instance.get('/info', {
+	params: {
+		limit: filters?.limit, 
+		offset: filters?.offset, 
+		emessage_name: filters?.emessage_name, 
+		date_start: filters?.date_start, 
+		date_end: filters?.date_end, 
+	}
+}))
 export const createBankInfo = (data) => (instance.post(`/info`, data))
 export const updateBankInfo = (id, data) => (instance.put(`/info/${id}`, data))
 export const removeBankInfo = (id) => (instance.delete(`/info/${id}`))
--- src/services/manuals-info.service.js
+++ src/services/manuals-info.service.js
@@ -1,6 +1,14 @@
 import { instance } from "./axios.config";
 
-export const getManualById = (id) => (instance.get(`/info/${id}/manuals`))
+export const getManualById = (id, filters) => (instance.get(`/info/${id}/manuals`, {
+	params: {
+		limit: filters?.limit,
+		offset: filters?.offset,
+		code: filters?.code,
+		description: filters?.description,
+		deleted: filters?.deleted
+	}
+}))
 export const createManual = (id, data) => (instance.post(`/info/${id}/manuals`, data))
 export const updateManual = (id, data) => (instance.put(`/info/manuals/${id}`, data))
 export const removeManual = (id) => (instance.delete(`/info/manuals/${id}`))
