5d96010000eec94b915d75417c0f912434cd9553
Matveyx25 <matveyx25@gmail.com>
2023-06-19 20:05:27 +0400

    Manuals events

--- src/modules/Manuals/Events/Events.jsx
+++ src/modules/Manuals/Events/Events.jsx
@@ -1,7 +1,16 @@
+import { Button } from '../../../components/Manuals/Button/Button'
 import s from './Events.module.scss'
+import { Add32Regular, ArrowCounterclockwise32Regular, ArrowReset32Regular, Dismiss32Regular, Edit32Regular, Print32Regular } from '@fluentui/react-icons'
 
 export const Events = () => {
 	return (
-		<div>Events</div>
+		<div className={s.wrapper}>
+			<Button content="Добавить" icon={<Add32Regular/>}/>
+			<Button content="Изменить"  icon={<Edit32Regular/>}/>
+			<Button content="Удалить" icon={<Dismiss32Regular/>}/>
+			<Button content="Восстановить" icon={<ArrowReset32Regular/>}/>
+			<Button content="Обновить" icon={<ArrowCounterclockwise32Regular/>}/>
+			<Button content="Печать" icon={<Print32Regular/>}/>
+		</div>
 	)
 }
